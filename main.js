181c28cbaa27c092627d52f0faea943b9774a0f1
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-03-10 07:43:10 -0500

    feat: Ejercicio 2

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
-alert ("A continuacion deberá introducir las 3 notas (de 0 a 5) que ha obtenido en el periodo para poder saber su promedio y si será becado")
+let num = Number(prompt("Introduzca su numero: "))
 
-a = true
+function par() {
+    return (num % 2) == 0
+}
 
-while (a = true) {
-    let nota1 = parseFloat(prompt("Ingrese su primera nota: "));
-    let nota2 = parseFloat(prompt("Ingrese su segunda nota: "));
-    let nota3 = parseFloat(prompt("Ingrese su tercera nota: "));
-
-    let resultado = ((nota1 + nota2 + nota3) / 3)
-
-    if (resultado <=0, resultado <=3.9){
-        alert(`Su promedio es de ${resultado}, estudie vago.`);
-        a = false
+if (par(num)){
+    if (num >= 10){
+        alert("Su numero es par y mayor a 10")
     }
-    else if (resultado >3.9, resultado <=5){
-        alert(`Su promedio es de ${resultado}, felicitaciones, ha sido becado.`)
-        a = false
+    if (num < 10){
+        alert("Su numero es par y menor a 10")
     }
-    else {
-        alert("Introduzca valores validos")
-    }
+}
+else {
+    alert("Su numero es impar")
 }
